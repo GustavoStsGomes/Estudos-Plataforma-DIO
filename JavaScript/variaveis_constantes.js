f32e2681a7286687b09290b8109049601f7db85c
Gustavo <gustavo.gomes.95@hotmail.com>
2022-06-02 17:16:18 -0300

    inicioJS

---
+++ JavaScript/variaveis_constantes.js
@@ -0,0 +1,14 @@
+// variaveis valores que podem mudar de valor ao longo do programa
+var valor=10;
+var desconto = 2;
+
+valor = valor - desconto;
+console.log(valor);
+
+//constantes são espaços separados para valores que não vão ser alterados por boas praticas com letras maisculas
+const VALOR =10;
+var desconto = 2;
+
+var total = VALOR - desconto;
+
+console.log(total);
